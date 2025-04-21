de70baff9c1bda47bc9dd72b6434d689e35a86a9
salsabila <salsabilselem9@gmail.com>
2025-04-21 17:24:53 +0200

    writing add function

--- STUCT/STUCT/1.cpp
+++ STUCT/STUCT/1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 #define MAX 100
+
+
+
 struct contact {
 	string name;
 	int mobile;
@@ -11,3 +14,34 @@ struct user {
 	string pass;
 	contact contacts[MAX];
 };
+
+void add()
+{
+	contact cont;
+	cout << "Enter the name : ";
+	cin >> cont.name;
+	cout << "Enter the Mobile Number : ";
+	cin >> cont.mobile;
+	cout << "Enter the E-mail : ";
+	cin >> cont.email;
+}
+
+int main()
+{
+	int choise;
+	cout << "Choose the Operation : \n";
+	cout << "1-Add\n";
+	cout << "2-Delete\n";
+	cout << "3-Edit\n";
+	cout << "4-Search\n";
+	cin >> choise;
+
+	switch (choise)
+	{
+	case 1:
+		add();
+		break;
+
+
+	}
+}
